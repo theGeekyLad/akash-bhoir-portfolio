78b06c3c9de7541c2ba175f70fb8911c0e9f731a
theGeekyLad <rahul.pillai03@gmail.com>
2026-06-29 19:56:16 -0700

    Add link to LOR

--- data/recommendations.js
+++ data/recommendations.js
@@ -5,7 +5,7 @@ window.recommendations = [
     company: "Almaden",
     relationship: "Worked with Akash for two years",
     contact: "vy.tran@almadenglobal.com",
-    documentUrl: "",
+    documentUrl: "https://docs.google.com/document/d/1fPrx-haZuWZzMHn1fLwnRS_wqDoBYZ3S/edit?usp=sharing&ouid=114841942227111596147&rtpof=true&sd=true",
     quote:
       "Akash is hardworking, reliable, and easy to work with. He communicates well with stakeholders and customers, learns quickly, adapts to new challenges, and is someone the team can always count on."
   }
